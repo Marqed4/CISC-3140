34767d1ea8ed44a750612908c90ba7e90e62dde8
Marqed <Derpexsplosion@gmail.com>
2026-05-12 11:39:26 -0400

    Connecting the dots...

--- Homework 7.js
+++ Homework 7.js
@@ -1,4 +1,19 @@
+// My defined func
 const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-html = html.replace('<p>Hello</p>', '<p>Hello MODIFIED</p>');
-console.log(html);
+
+function fetchLine() {
+    return fs.readFileSync('index.html', 'utf8');
+}
+
+function modifyLine(html) {
+    return html.replace('<h2>2001</h2>', '<h2>2002</h2>');
+}
+
+function printLine(html) {
+    console.log(html);
+}
+
+// Main
+let html = fetchLine();
+html = modifyLine(html);
+printLine(html);
